04ea56cbad82bb305eb50f4e7719087897cc7a7f
Krishnajha25 <krishnajha366@gmail.com>
2020-08-13 00:41:07 +0530

    app.js file updated with middleware

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 const router = require('./routes/index')
 
 //Load config
 dotenv.config({path: './config/config.env'})
+
+//Passport config
+require('./config/passport')(passport)
 
 connectDB()
 const app = express()
@@ -16,10 +21,21 @@ if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
-//Hnadlebars
-
+//Handlebars
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
+
+//Session
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}))
+
+//Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')))
